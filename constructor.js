f443cee317336cf36487271d6d1a76d96bcdf5ea
firas-ibraheem <ferasibraheem2@gmail.com>
2022-08-03 16:04:13 +0300

    change

---
+++ constructor.js
@@ -0,0 +1,11 @@
+// function employees ( employeeID,fullName,imgUrl,level,salary){
+//     this.employeeID = employeeID;
+//     this.fullName = fullName;
+//     this.imgUrl = imgUrl;
+//     this.level = level;
+//     this.salary = salary;
+
+
+
+
+// }
