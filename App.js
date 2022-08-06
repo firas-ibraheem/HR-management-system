66d6c06a6ad31bbf3f477d5066a255c61ef22e20
firas-ibraheem <ferasibraheem2@gmail.com>
2022-08-07 02:29:16 +0300

    add

--- App.js
+++ App.js
@@ -1,38 +1,77 @@
-'use strict';
 
-function Employees ( employeeID,fullName,imgUrl,Department,level,salary){
+
+const main = document.getElementsByTagName('body');
+
+
+
+function Employees ( employeeID,fullName,imgUrl,Department,level){
     this.employeeID = employeeID;
     this.fullName = fullName;
     this.imgUrl = imgUrl;
     this.Department = Department;
     this.level = level;
-    this.salary = salary;
     this.uniqId = function(counter){
         this.EmployeeID = counter +1;
     ++Id;
     },
-    this.taxedsal = function(min,max){
-        let salary = Math.floor(Math.random()*(max- min)+ min);
-        taxedSalary = salary - (salary*0.075); 
-        this.salary = taxedSalary;
+    this.salary = function(){
+        let min = 0;
+        let max = 0;
+        let salary1 = 0;
+        let taxedSalary = 0;
+        if (this.level == "Senior"){
+            min = 1500;
+            max = 2000;
+            salary1 = Math.floor(Math.random()*(max- min)+ min);
+            return salary1;
+        } else if (this.level == "Mid-Senior" ){
+             min = 1000;
+             max = 1500;
+             salary1 = Math.floor(Math.random()*(max- min)+ min);
+            return salary1;
+
+        }else {
+             min = 500;
+             max = 1000;
+             salary1 = Math.floor(Math.random()*(max- min)+ min);
+            return salary1;
+        
+        }
+       
+        taxedSalary = salary1 - (salary1*0.075); 
+        return taxedSalary;
        
     }
 
 }
 Employees.prototype.render = function(){
-    document.write("Employee name:",this.fullName);
-    document.write("Department:",this.Department);
-    document.write("Employee salary",this.taxedsal);
+    
+    const imgEl = document.createElement('img');
+    imgEl.src = this.imgUrl;
+    imgEl.alt = this.fullName;
+    main[0].appendChild(imgEl);
+    
+    const divEl1 = document.createElement('div');
+    divEl1.textContent =`Name : ${this.fullName}`;
+    const divEl2 = document.createElement('div');
+    divEl2.textContent =`Department : ${this.Department}`;
+    const divEl3 = document.createElement('div');
+    // divEl3.textContent =`Salray : ${this.salary}`;
+    main[0].appendChild(divEl1);
+    main[0].appendChild(divEl2);
+    main[0].appendChild(divEl3);
+
+    
 
 }
 
-const emp1 = new Employees(this.uniqId,"Ghazi Samer",'https://cdnblog.filecloud.com/blog/wp-content/uploads/2018/04/administrator1.jpg',"Administration","Senior",this.taxedsal);
-const emp2 = new Employees(this.uniqId,"Lana Ali",'https://image.shutterstock.com/image-photo/business-concept-two-mans-working-600w-1116352550.jpg',"Finance","Senior",this.taxedsal);
-const emp3 = new Employees(this.uniqId,"Tamara Ayoub",'https://media.istockphoto.com/photos/digital-marketing-concept-online-advertisement-picture-id1284549946?s=612x612',"Marketing","Senior",this.taxedsal);
-const emp4 = new Employees(this.uniqId,"Safi Walid",'https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2020/07/03070917/system-administrator-job-description-and-career-1024x512.jpg',"Administration","Mid-Senior",this.taxedsal);
-const emp5 = new Employees(this.uniqId,"Omar Zaid",'https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562',"Development","Senior",this.taxedsal);
-const emp6 = new Employees(this.uniqId,"Rana Saleh",'https://media.istockphoto.com/photos/computer-programmer-working-on-new-software-program-picture-id1212006391?s=612x612',"Development","Junior",this.taxedsal);
-const emp7 = new Employees(this.uniqId,"Hadi Ahmad",'https://image.shutterstock.com/image-photo/woman-accountant-use-calculator-computer-600w-1697077897.jpg',"Finance","Mid-Senior",this.taxedsal);
+const emp1 = new Employees(this.uniqId,"Ghazi Samer",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Ghazi.jpg',"Administration","Senior",this.taxedsal);
+const emp2 = new Employees(this.uniqId,"Lana Ali",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Lana.jpg',"Finance","Senior",this.taxedsal);
+const emp3 = new Employees(this.uniqId,"Tamara Ayoub",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Tamara.jpg',"Marketing","Senior",this.taxedsal);
+const emp4 = new Employees(this.uniqId,"Safi Walid",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Safi.jpg',"Administration","Mid-Senior",this.taxedsal);
+const emp5 = new Employees(this.uniqId,"Omar Zaid",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Omar.jpg',"Development","Senior",this.taxedsal);
+const emp6 = new Employees(this.uniqId,"Rana Saleh",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Rana.jpg',"Development","Junior",this.taxedsal);
+const emp7 = new Employees(this.uniqId,"Hadi Ahmad",'https://raw.githubusercontent.com/LTUC/new-prep-course-std/main/Day10/Task/assets/Hadi.jpg',"Finance","Mid-Senior",this.taxedsal);
 
 emp1.render();
 emp2.render();
